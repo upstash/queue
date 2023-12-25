1719d9b5c6072f2dda50b58f259124ded01969a8
ogzhanolguncu <ogzhan11@gmail.com>
2023-12-25 17:41:20 +0300

    Add polling with block

--- queue.test.ts
+++ queue.test.ts
@@ -1,35 +1,87 @@
-import { beforeEach, expect, test } from "bun:test";
+import { sleep } from "bun";
+import { getRandomSeed } from "bun:jsc";
+import { describe, expect, test } from "bun:test";
 import { Redis } from "ioredis";
 import { Queue } from "./queue";
 import { formatMessageQueueKey } from "./utils";
-import { sleep } from "bun";
 
+const randomValue = getRandomSeed();
 const redis = new Redis();
+const consumerClient = new Redis();
 
-// test("should add item to queue", async () => {
-//   const queue = new Queue({ redis, queueName: "app-logs" });
+describe("Queue", () => {
+  test("should add item to queue", async () => {
+    const queue = new Queue({ redis, queueName: "app-logs" });
 
-//   const sendMessageResult = (await queue.sendMessage({
-//     dev: "hezarfennnn",
-//     age: 27,
-//   })) as string;
+    const sendMessageResult = (await queue.sendMessage({
+      dev: "hezarfennnn",
+      age: 27,
+    })) as string;
 
-//   const res = await redis.xrevrange(
-//     formatMessageQueueKey("app-logs"),
-//     "+",
-//     "-",
-//     "COUNT",
-//     1
-//   );
-//   expect(res[0][0]).toEqual(sendMessageResult);
-// });
-
-test("should try to read from stream with consumer group", async () => {
-  const queue = new Queue({ redis, queueName: "app-logs" });
-  const sendMessageRes = await queue.sendMessage({
-    dev: "hezarfen",
-    age: 27,
+    const res = await redis.xrevrange(
+      formatMessageQueueKey("app-logs"),
+      "+",
+      "-",
+      "COUNT",
+      1
+    );
+    await redis.xdel(formatMessageQueueKey("app-logs"), res[0][0]);
+    expect(res[0][0]).toEqual(sendMessageResult);
   });
-  const receiveMessageRes = await queue.receiveMessage("customConsumerName");
-  expect(sendMessageRes).toEqual(receiveMessageRes?.streamId ?? "");
+
+  test(
+    "should enqueue with a delay",
+    async () => {
+      const queue = new Queue({ redis, queueName: "app-logs" });
+      await queue.sendMessage(
+        {
+          dev: randomValue,
+        },
+        2
+      );
+
+      await sleep(5000);
+      const res = await redis.xrevrange(
+        formatMessageQueueKey("app-logs"),
+        "+",
+        "-",
+        "COUNT",
+        1
+      );
+      expect(res[0][1]).toEqual(["messageBody", `{\"dev\":${randomValue}}`]);
+    },
+    { timeout: 10000 }
+  );
+
+  test("should try to read from stream with consumer group", async () => {
+    const queue = new Queue({ redis, queueName: "app-logs" });
+    const receiveMessageRes = await queue.receiveMessage("consumer-1");
+
+    expect(receiveMessageRes?.streamId ?? "").toBeTruthy();
+  });
+
+  test(
+    "should poll until data arives",
+    async () => {
+      const producer = new Queue({ redis, queueName: "app-logs" });
+      const consumer = new Queue({
+        redis: consumerClient,
+        queueName: "app-logs",
+      });
+      await producer.sendMessage(
+        {
+          dev: randomValue,
+        },
+        2
+      );
+
+      const receiveMessageRes = await consumer.receiveMessage(
+        "consumer-1",
+        5000
+      );
+
+      expect(receiveMessageRes?.body.messageBody.dev).toEqual(randomValue);
+    },
+    { timeout: 10000 }
+  );
 });
--- queue.ts
+++ queue.ts
@@ -2,7 +2,11 @@ import { EventEmitter } from "events";
 import Redis from "ioredis";
 import { formatMessageQueueKey } from "./utils";
 
+const MAX_RETRY_COUNT = 3; // Maximum number of retries
+const RETRY_DELAY_MS = 1500; // Delay between retries in milliseconds
+
 const CONSUMER_GROUP_NAME = "messages";
+
 export type QueueConfig = {
   redis: Redis;
   queueName: string;
@@ -10,6 +14,7 @@ export type QueueConfig = {
 
 export class Queue extends EventEmitter {
   config: QueueConfig;
+  messageTimeouts = new Set();
 
   constructor(config: QueueConfig) {
     super();
@@ -44,54 +49,68 @@ export class Queue extends EventEmitter {
     }
   }
 
-  async sendMessage<T extends {}>(payload: T) {
+  async sendMessage<T extends {}>(payload: T, delayInSeconds: number = 0) {
     const { redis } = this.config;
     try {
       const flattenedPayload = Object.entries({
         messageBody: JSON.stringify(payload),
       }).flat() as string[];
 
-      const res = await redis.xadd(
-        this.createStreamKey(),
-        "*",
-        ...flattenedPayload
-      );
-      return res;
+      const streamKey = this.createStreamKey();
+
+      if (delayInSeconds > 0) {
+        const timeoutId = setTimeout(() => {
+          redis
+            .xadd(streamKey, "*", ...flattenedPayload)
+            .then((res) => {
+              this.messageTimeouts.delete(timeoutId);
+            })
+            .catch((error) => {
+              console.error("Error in delayed xadd:", error);
+              this.emit("error", error);
+            });
+        }, delayInSeconds * 1000);
+        this.messageTimeouts.add(timeoutId);
+      } else {
+        return await redis.xadd(streamKey, "*", ...flattenedPayload);
+      }
     } catch (error) {
       this.emit("error", error);
       console.error("Error in sendMessage:", error);
     }
   }
 
-  async receiveMessage(customConsumerName: string) {
+  async receiveMessage(consumerName: string, blockTimeMs = 5000) {
     const { redis } = this.config;
     try {
       const res = await redis.xreadgroup(
         "GROUP",
         CONSUMER_GROUP_NAME,
-        customConsumerName,
+        consumerName,
         "COUNT",
         1,
+        "BLOCK",
+        blockTimeMs,
         "STREAMS",
         this.createStreamKey(),
         ">"
       );
-
-      //TODO: type should be fixed
-      const resultBody = res[0][1][0] as [string, [string, string]];
-
-      const resultObject = {
-        streamId: resultBody[0],
-        body: {
-          [resultBody[1][0]]: JSON.parse(resultBody[1][1]),
-        },
-      };
-      if (resultObject) {
-        //TODO: apply retry here. if retry also fails release this
+      //@ts-ignore
+      if (res && res.length > 0 && res[0][1].length > 0) {
+        // Message found, process and return
+        //@ts-ignore
+        const resultBody = res[0][1][0] as [string, [string, string]];
+        const resultObject = {
+          streamId: resultBody[0],
+          body: {
+            [resultBody[1][0]]: JSON.parse(resultBody[1][1]),
+          },
+        };
         await this.verifyDelivery(redis, resultObject);
+        return resultObject;
       }
 
-      return resultObject;
+      return null;
     } catch (error) {
       console.log(`receiveMessage ${(error as Error).message}`);
     }
@@ -101,15 +120,32 @@ export class Queue extends EventEmitter {
     redis: Redis,
     resultObject: { streamId: string; body: { [x: string]: any } }
   ) {
-    try {
-      return await redis.xack(
-        this.createStreamKey(),
-        CONSUMER_GROUP_NAME,
-        resultObject.streamId
-      );
-    } catch (error) {
-      this.emit("error", error);
-      console.error("Error in verifyDelivery:", error);
-    }
+    let retryCount = 0;
+
+    const attemptAck = async (): Promise<boolean> => {
+      try {
+        await redis.xack(
+          this.createStreamKey(),
+          CONSUMER_GROUP_NAME,
+          resultObject.streamId
+        );
+        return true;
+      } catch (error) {
+        if (retryCount < MAX_RETRY_COUNT) {
+          retryCount++;
+          console.log(
+            `Retrying acknowledgment (${retryCount}/${MAX_RETRY_COUNT}).`
+          );
+          await new Promise((resolve) => setTimeout(resolve, RETRY_DELAY_MS));
+          return attemptAck();
+        } else {
+          this.emit("error", error);
+          console.error("Final attempt failed in verifyDelivery:", error);
+          return false;
+        }
+      }
+    };
+
+    return attemptAck();
   }
 }
