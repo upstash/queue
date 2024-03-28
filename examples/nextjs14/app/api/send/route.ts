8db8d69446970db6b9b3b49efe1e00b453d4ab34
fahreddinozcan <ozcanfahrettinn@gmail.com>
2024-03-28 05:20:49 +0300

    remove redundant comment

--- examples/nextjs14/app/api/send/route.ts
+++ examples/nextjs14/app/api/send/route.ts
@@ -1,11 +1,9 @@
-// import { Queue } from "../../../../../src";
 import { Queue } from "@upstash/queue";
 import { Redis } from "@upstash/redis";
 
 const redis = new Redis({
   url: process.env.UPSTASH_REDIS_REST_URL as string,
   token: process.env.UPSTASH_REDIS_REST_TOKEN as string,
-
 });
 
 export async function POST(req: Request) {
